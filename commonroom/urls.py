e90973e07305f28246198d41c1c47916e0fea66e
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2021-11-15 13:08:52 +0100

    Initial home page design

--- commonroom/urls.py
+++ commonroom/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 
 from commonroom.views import index
@@ -22,3 +24,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns = staticfiles_urlpatterns() + urlpatterns
