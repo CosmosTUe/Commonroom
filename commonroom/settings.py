9e0ae2cd0efc4d9c7f60c252054ac9780282f611
Maxime Kabel <maxime.kabel001@gmail.com>
2022-01-15 18:19:17 +0100

    Pre commit, fix flake8 formatting

--- commonroom/settings.py
+++ commonroom/settings.py
@@ -10,10 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 import secret_settings
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
